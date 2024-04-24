9a371458f0413cb6fef5e4d5265d387004663e76
djhoggatt <djhoggatt@gmail.com>
2024-04-23 19:12:21 -0600

    Added System Verilog templates

---
+++ template.sv
@@ -0,0 +1,18 @@
+// template.sv
+// Description of component
+
+
+// Test Module
+module template();
+
+    // I/O
+  
+    // Constants
+
+    // Registers
+  
+    // Assignment Logic
+
+    // Transitional Logic
+
+endmodule
---
+++ templateTestBench.sv
@@ -0,0 +1,31 @@
+// templateTestBench.sv
+// Description of test
+
+
+// Test Module
+module testBench;
+
+    // I/O
+  
+    // Instantiation
+    template TEMPLATE();
+  
+    // Initial setup
+    initial
+    begin
+
+    end
+
+    // Test
+    initial
+    begin
+
+    end
+  
+    // Tasks
+    task templateTask;
+    begin
+
+    end
+
+endmodule
